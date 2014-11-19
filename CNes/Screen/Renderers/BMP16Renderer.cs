d3f825ae6116a9ef29d0e9c9e9da78235036aff3
Alex Restifo <calix1999@gmail.com>
2014-11-19 15:49:41 -0500

    Ugh

--- CNes/Screen/Renderers/BMP16Renderer.cs
+++ CNes/Screen/Renderers/BMP16Renderer.cs
@@ -22,28 +22,28 @@ namespace CNes.Screen.Renderers
 
         int height;
         const int width = 256;
-
+        
         public BMP16Renderer(Cart c)
         {
             this.c = c;
             InitRenderer();
         }
 
-        public Image Render(byte[] input, ushort curScanline) //Byte array parameter is the input array for the scanline being passed to the renderer
+        public unsafe Image Render(byte[] input, ushort curScanline) //Byte array parameter is the input array for the scanline being passed to the renderer
         {
-            //Create better renderer later, but now we're just going to use some ghetto monochrome half-assed custom renderer.
-            for (int l = 0; l < input.Length; l++) //I have reasons to not use a for-each
-            {
-                if (input[l] != 0)
-                {
-                    //This assumes scanlines go vertically, but maybe they go horizontally (would be a very easy fix)
-                    Console.WriteLine("l: " + l + " output of logic: " + (l % 256 == 0 && l != 0 ? l / 256 : (l - (l % 256)) / 256));
-                    bmp.SetPixel(curScanline, l % 256 == 0 && l != 0 ? l / 256 : (l - (l % 256)) / 256, Color.Black);
-                }
-            }
-            /* TEST */ Console.WriteLine(257 / 256);
+            ////Create better renderer later, but now we're just going to use some ghetto monochrome half-assed custom renderer.
+            //for (int l = 0; l < input.Length; l++) //I have reasons to not use a for-each
+            //{
+            //    if (input[l] != 0)
+            //    {
+            //        //This assumes scanlines go vertically, but maybe they go horizontally (would be a very easy fix)
+            //        Console.WriteLine("l: " + l + " output of logic: " + (l % 256 == 0 && l != 0 ? l / 256 : (l - (l % 256)) / 256));
+            //        bmp.SetPixel(curScanline, l % 256 == 0 && l != 0 ? l / 256 : (l - (l % 256)) / 256, Color.Black);
+            //    }
+            //}
+            ///* TEST */ Console.WriteLine(257 / 256);
 
-            return bmp; //Returns the image to be used in the picturebox???????
+            //return bmp; //Returns the image to be used in the picturebox???????
         }
 
         public void InitRenderer()
